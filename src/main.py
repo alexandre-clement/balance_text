b7178e9543975c376591106e93c4302a86ac5695
Alexandre Clement <alexandre.clement@etu.unice.fr>
2018-03-19 09:59:18 +0100

    Correction d'un bug du calcul du score sur la première ligne.

--- src/main.py
+++ src/main.py
@@ -10,24 +10,36 @@ def process(text, width):
     words = text.split()
     size = [len(w) for w in words]
 
-    dynamic: List[List[Solution]] = [[Solution(0, 0, 0, 0, 0, None)]]
+    best_solution = final_solution = Solution(0, 0, width, 0, -1, None)
+    dynamic: List[List[Solution]] = [[best_solution]]
 
     for i, s in enumerate(size):
         temp = []
-        min_score = min(dynamic[-1]).total_score
+        new_min_score = final_solution = Solution(float('inf'), 0, 0, 0, 0, None)
         for j, solution in enumerate(dynamic[-1]):
             if solution.line_width + s + 1 <= width:
                 line_width = solution.line_width + s + 1
                 line_score = (width - line_width) ** 2
                 total_score = solution.total_score - solution.line_score + line_score
-                temp.append(Solution(total_score, line_score, line_width, i, solution.line, solution))
-            if solution.total_score == min_score:
+                new_solution = Solution(total_score, line_score, line_width, i, solution.line, solution)
+                if total_score < new_min_score.total_score:
+                    new_min_score = new_solution
+                if total_score - line_score < final_solution.total_score:
+                    final_solution = Solution(total_score - line_score, 0, line_width, i, solution.line, solution)
+                temp.append(new_solution)
+            if solution.total_score == best_solution.total_score:
                 line_score = (width - s) ** 2
                 total_score = solution.total_score + line_score
-                temp.append(Solution(total_score, line_score, s, i, solution.line + 1, solution))
+                new_solution = Solution(total_score, line_score, s, i, solution.line + 1, solution)
+                if total_score < new_min_score.total_score:
+                    new_min_score = new_solution
+                if total_score - line_score < final_solution.total_score:
+                    final_solution = Solution(solution.total_score, 0, s, i, solution.line + 1, solution)
+                temp.append(new_solution)
+        best_solution = new_min_score
         dynamic.append(temp)
 
-    solution = tmp = min(dynamic[-1])
+    solution = tmp = final_solution
     result = [[] for _ in range(tmp.line + 1)]
     while tmp.parent is not None:
         result[tmp.line].insert(0, words[tmp.word])
